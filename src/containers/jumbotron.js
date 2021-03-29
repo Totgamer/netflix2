5072b796b6f6c20ad0ed44d6c2d225407bf39714
Marco Molenaar <532548@student.glu.nl>
2021-03-29 17:06:50 +0200

    subTitles

--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -10,7 +10,7 @@ export function JumbotronContainer() {
         <Jumbotron key={item.id} direction={item.direction}>
           <Jumbotron.Pane>
             <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.SubTitle}</Jumbotron.SubTitle>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
           </Jumbotron.Pane>
           <Jumbotron.Pane>
             <Jumbotron.Image src={item.image} alt={item.alt} />
