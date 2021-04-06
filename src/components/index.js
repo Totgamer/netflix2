0b82eac5f5d53ec342b9787597eacf3057c7b107
Marco Molenaar <532548@student.glu.nl>
2021-04-06 19:42:55 +0200

    Q&A component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Accordion } from './accordion';
 export { default as Jumbotron } from './jumbortron';
-export { default as Footer } from './footer'
+export { default as Footer } from './footer';
+export { default as OptForm } from './opt-form';
