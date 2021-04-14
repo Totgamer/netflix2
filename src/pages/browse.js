1a412bf15b044054b51cd49b88c33ab226b83a26
Marco Molenaar <532548@student.glu.nl>
2021-04-14 22:45:52 +0200

    profiles
    Co-authored-by: beau-vr <beau-vr@users.noreply.github.com>

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowseContainer } from '../containers/browse';
 import { useContent } from '../hooks'; 
+import selectionFilter  from '../utils/selection-filter'
 
 export default function Browse() {
-    // const { series } = useContent('series');
-    // const { films } = useContent('films');
+    
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+
+    const slides = selectionFilter({ series, films });
 
     
-    return <p>Hello from the browse!</p>;
+    return (
+        <BrowseContainer slides={slides} />
+    );
 }
