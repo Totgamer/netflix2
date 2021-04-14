223fbba56023e7af748e7a926c64fdbb4826b65d
Marco Molenaar <532548@student.glu.nl>
2021-04-14 16:32:36 +0200

    bugs user + bugs typo's

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useContent } from '../hooks'; 
-import useContext from '../hooks/use-content';
 
 export default function Browse() {
-    const { series } = useContext('series');
-    const { films } = useContext('films');
+    // const { series } = useContent('series');
+    // const { films } = useContent('films');
 
     
     return <p>Hello from the browse!</p>;
